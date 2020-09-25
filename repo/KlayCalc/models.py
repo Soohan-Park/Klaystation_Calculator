4594c856a20b989d3cbd29a64b5ecbd1409ae304
Soohan Park <725psh@gmail.com>
2020-09-25 14:50:39 +0900

    Update models

--- repo/KlayCalc/models.py
+++ repo/KlayCalc/models.py
@@ -4,23 +4,33 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
 
-    def __str__(self):
-        return self.question_text
+# Example.
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+#     def __str__(self):
+#         return self.question_text
+
+#     def was_published_recently(self):
+#         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
 
 
-    def __str__(self):
-        return self.choice_text
-    
+#     def __str__(self):
+#         return self.choice_text
+
+
+# Not Yet
+class Donation(models.Model):
+    no = models.IntegerField()
+    name = models.CharField(max_length=20)
+    amount = models.IntegerField(default=0)
+    donateDate = models.DateTimeField('Date of Donate')
+    pass
