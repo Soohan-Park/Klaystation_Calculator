f5f7cc8e7ff592c876884c703a8c8d4d455919da
Soohan Park <725psh@gmail.com>
2020-09-26 23:07:31 +0900

    Add .js files & Modified sidebar

---
+++ repo/KlayCalc/static/js/donate.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('#sidebar_donate').addClass('active')
+    $('#navbar_title').text('Donate')
+    $('#navbar_title').click( function () {
+        alert('이스터에그')
+    } )
+})
---
+++ repo/KlayCalc/static/js/index.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('#sidebar_index').addClass('active')
+    $('#navbar_title').text('Index')
+    $('#navbar_title').click( function () {
+        alert('이스터에그')
+    } )
+})
