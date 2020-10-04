d31f348d78a0d8d59013074b8b567669d39779a0
Soohan Park <725psh@gmail.com>
2020-10-04 16:50:32 +0900

    Add DataBase

--- repo/KlayCalc/admin.py
+++ repo/KlayCalc/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Donation
+from .models import Donation, RateData
 
 # Register your models here.
 admin.site.register(Donation)
+admin.site.register(RateData)
